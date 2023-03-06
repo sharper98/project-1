4aaafb650bb578c6a50e86af2eea65f868bfa424
Sean H <sharper1006@hotmail.com>
2023-03-06 15:30:07 -0500

    Spotify's API info

--- README2.md
+++ README2.md
@@ -1,2 +1,7 @@
 # Simple Spotify Album Searcher
 I'm extremely passionate about music, so my initial idea for this project was to integrate Spotify's API and create an app that could be used as a place to search fore and review music. I soon realized that I bit off more than I could chew and ended up making a mess of code. I ended up scrapping the original plan and decided to start with something smaller that would just be basically a search engine for Spotify albums. From there I plan on continue working on this app until it becomes my own Letterboxd but for music.
+
+# Using Spotify's API
+Spotify allows developers to use their API for free, all you need is a Spotify account (it can be premium or free).
+Use developer.spotify.com/dashboard to get started
+From there you can navigate their site and create an app, once done it'll take you to your app's overview page which can provide the Client ID, user access, and data regarding your apps user activity.
--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, InputGroup, FormControl, Button, Row, Card } from 'react-bootstrap';
+import { useState, useEffect } from 'react';
+
+const CLIENT_ID = "c4dd0f9f5c94455399469234ebb435a2"
+const CLIENT_SECRET = "43971aa35f7c443fa5958161b2ccd96c"
 
 function App() {
+  const [searchInput, setSearchInput] = useState('');
+  const [accessToken, setAccessToken] = useState("");
+  const [albums, setAlbums] = useState([]);
+
+  useEffect(() => {
+    // API Access Token
+    var authParameters = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
+    }
+    fetch('https://accounts.spotify.com/api/token', authParameters)
+      .then(result => result.json())
+      .then(data => setAccessToken(data.access_token))
+
+  }, [])
+
+  // Search
+  async function search() {
+    console.log("Search for " + searchInput);
+  
+  // Get request using search to get Artist ID
+  var searchParameters = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + accessToken
+    }
+  }
+  var artistID = await fetch('https://api.spotify.com/v1/search?q=' + searchInput + '&type=artist', searchParameters)
+   .then(response => response.json())
+   .then(data => { return data.artists.items[0].id })
+
+   console.log("Artist ID is " + artistID);
+  // Get request with Artist ID grab all the albums from that artist
+  var returnedAlbums = await fetch('https://api.spotify.com/v1/artists/' + artistID + '/albums' + '?include_groups=album&market=US&limit=50', searchParameters)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      setAlbums(data.items);
+    });
+  // Display those albums to the user
+  }
+  console.log(albums);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <InputGroup className="mb-3" size="lg">
+          <FormControl
+            placeholder="Search For Artist"
+            type="input"
+            onKeyPress={event => {
+              if (event.key === 'Enter') {
+                search();
+              }
+            }}
+            onChange={event => setSearchInput(event.target.value)}
+          />
+          <Button onClick={search}>
+            Search
+          </Button>
+        </InputGroup>
+
+      </Container>
+      <Container>
+        <Row className="mx-2 row row-cols-4">
+          {albums.map( (album, i) => {
+            console.log(album);
+            return (
+              <Card>
+                <Card.Img src={album.images[0].url} />
+                <Card.Body>
+                  <Card.Title>{album.name}</Card.Title>
+                </Card.Body>
+              </Card>
+             )
+           })}
+
+        </Row>
+      </Container>
+        
     </div>
-  );
+    );
 }
 
 export default App;
